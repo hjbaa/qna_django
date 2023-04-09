21819f837a978033c6d38c8ee65967eba9982c0a
Skandakov <egor.skandakov@gmail.com>
2023-04-10 02:13:17 +0300

    Added application, added base.html view file

--- askme_django/settings.py
+++ askme_django/settings.py
@@ -59,8 +59,7 @@ ROOT_URLCONF = 'askme_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
