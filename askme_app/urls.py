9648592b3eb374095afe04c6dc1295e468e67aae
Skandakov <egor.skandakov@gmail.com>
2023-04-20 02:09:52 +0300

    Added urls.py file to application

---
+++ askme_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from askme_app import views
+
+urlpatterns = [
+    path('question/<int:question_id>', views.show_question, name="question"),
+    path('tag/<str:title>', views.show_by_tag, name="by_tag"),
+    path('hot/', views.hot, name="hot_questions"),
+    path('ask/', views.new_question, name='ask_question'),
+]
+
+# вынести юрлы в приложение
